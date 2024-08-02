35fb98f9e2fd9e5dce2e9b32870f2c7d91fd2d66
Ninh <khanhninhsp@gmail.com>
2024-08-02 23:17:46 +0200

    writing README.md

--- tests/testthat/test-attribution.R
+++ tests/testthat/test-attribution.R
@@ -6,38 +6,23 @@ library(testthat)
 predictive_models = readRDS(file = system.file("extdata", "PredictiveRule.RDS",
                                                package = "attr"))
 
-dataset <- get(load(file = system.file("extdata", "dataset.RData",
-                                       package = "attr")))
-
-dataset <- lapply(dataset, function(x) {
-  names(x) <- c("date", "GE", "GGp", "GEp", "EEp", "GpEp", "GpE")
-  return(x)
-})
+dataset <- readRDS(file = system.file("extdata", "dataset.RDS",
+                                       package = "attr"))
 
 # add change-points to test
+convert_dates <- function(dates_list) {
+  lapply(dates_list, function(dates) as.Date(dates, format = "%Y-%m-%d"))
+}
 
-main_cp = as.Date( c("2016-04-04"),
-                      format = "%Y-%m-%d")
-nearby_cp = list(
-  bces = as.Date( c("2010-02-25",
-                    "2012-08-13",
-                    "2017-06-19"),
-                  format = "%Y-%m-%d"),
-  bcns = as.Date( c("2010-02-24",
-                    "2011-12-17",
-                    "2012-08-18",
-                    "2017-06-29"),
-                  format = "%Y-%m-%d"),
-  pgc5 =  as.Date( c( "2009-07-24",
-                      "2011-12-17",
-                      "2017-11-20"),
-                   format = "%Y-%m-%d"),
-  ptaa = as.Date( c(),
-                  format = "%Y-%m-%d"),
-  p435 = as.Date( c("2011-10-12"),
-                  format = "%Y-%m-%d")
-)
+main_cp <- as.Date("2016-04-04", format = "%Y-%m-%d")
 
+nearby_cp <- convert_dates(list(
+  bces = c("2010-02-25", "2012-08-13", "2017-06-19"),
+  bcns = c("2010-02-24", "2011-12-17", "2012-08-18", "2017-06-29"),
+  pgc5 = c("2009-07-24", "2011-12-17", "2017-11-20"),
+  ptaa = c(),
+  p435 = c("2011-10-12")
+))
 
 # Premilinary test --------------------------------------------------------
 ## Test if is there any cluster (group of change points within a period e.g, 80 days)
